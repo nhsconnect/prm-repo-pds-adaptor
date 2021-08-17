cd70336cc840059ceecf41af90aab614d1a167b6
Jack Slater <jslater@thoughtworks.com>
2021-08-17 16:24:49 +0100

    [PRMT-2100] write test to verify health check

--- src/test/java/uk/nhs/prm/deductions/pdsadaptor/PrmDeductionsPdsAdaptorApplicationTests.java
+++ src/test/java/uk/nhs/prm/deductions/pdsadaptor/PrmDeductionsPdsAdaptorApplicationTests.java
@@ -1,13 +1,25 @@
 package uk.nhs.prm.deductions.pdsadaptor;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PrmDeductionsPdsAdaptorApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void applicationIsHealthy() {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/actuator/health",
+            String.class)).isEqualTo("{\"status\":\"UP\"}");
+    }
 }
