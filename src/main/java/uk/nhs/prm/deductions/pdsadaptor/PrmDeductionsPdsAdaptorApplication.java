4eb3ba564008d4bda22f37fd0100673133c2e389
Jack Slater <jslater@thoughtworks.com>
2021-08-17 15:44:21 +0100

    [PRMT-2100] initial commit

---
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/PrmDeductionsPdsAdaptorApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.prm.deductions.pdsadaptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrmDeductionsPdsAdaptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrmDeductionsPdsAdaptorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/nhs/prm/deductions/pdsadaptor/PrmDeductionsPdsAdaptorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.nhs.prm.deductions.pdsadaptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrmDeductionsPdsAdaptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
