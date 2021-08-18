4382f68ac44ccdc21288bc44abddbeb73d38f116
Jack Slater <jslater@thoughtworks.com>
2021-08-18 09:24:00 +0100

    [PRMT-2100] split integration tests to separate directory source

---
+++ src/integrationtest/java/uk/nhs/prm/deductions/pdsadaptor/ApplicationIntegrationTest.java
@@ -0,0 +1,25 @@
+package uk.nhs.prm.deductions.pdsadaptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void applicationIsHealthy() {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/actuator/health",
+            String.class)).isEqualTo("{\"status\":\"UP\"}");
+    }
+}
