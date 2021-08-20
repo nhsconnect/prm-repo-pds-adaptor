0244b6e8b1ecf2912b54af8cc5ec97c20a849a5f
Jack Slater <jslater@thoughtworks.com>
2021-08-20 21:01:53 +0100

    [PRMT-2100] add servers url to swagger config

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/SwaggerConfig.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/SwaggerConfig.java
@@ -4,10 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "PDS Adaptor", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "PDS Adaptor", version = "v1"), servers = @Server(url = "/"))
 @SecurityScheme(name = "apiKey", type = SecuritySchemeType.APIKEY)
 public class SwaggerConfig {
 }
