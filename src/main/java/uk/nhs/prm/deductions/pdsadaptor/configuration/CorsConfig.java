5cd8207d98f958cee3dedad5e3d39f288badc7c5
Jack Slater <jslater@thoughtworks.com>
2021-08-20 15:36:58 +0100

    [PRMT-2100] add cors config

---
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/CorsConfig.java
@@ -0,0 +1,13 @@
+package uk.nhs.prm.deductions.pdsadaptor.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "OPTIONS");
+    }
+}
