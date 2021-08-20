aca45f222fd021f96408c37d6684b27febc72e71
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-08-20 15:07:15 +0100

    [PRMT-2100] Added Cross origin annotation.

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/controller/SampleController.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/controller/SampleController.java
@@ -1,6 +1,7 @@
 package uk.nhs.prm.deductions.pdsadaptor.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
     @GetMapping("/{name}")
+    @CrossOrigin
     public String healthCheck(@PathVariable String name) {
         return "Hello " + name;
     }
