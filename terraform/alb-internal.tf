f49d6e8f76ff52872103e1720d76d7d61bdc0413
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-08-24 15:59:34 +0100

    [PRMT-2323] Updated alb to ecs security group to have correct tag

--- terraform/alb-internal.tf
+++ terraform/alb-internal.tf
@@ -146,7 +146,7 @@ resource "aws_security_group" "alb_to_pds_adaptor_ecs" {
   }
 
   tags = {
-    Name = "${var.environment}-alb-to-${var.component_name}-ecr"
+    Name = "${var.environment}-alb-to-${var.component_name}-ecs"
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
