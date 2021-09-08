7e20ba95e32e9de2302b4bd43758b4144440359a
syedsadiqali <syed.ali69@nhs.net>
2021-09-08 17:31:55 +0100

    [PRMT-2329] Added try/catch for RestTemplate Exceptions

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/auth/AuthService.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/auth/AuthService.java
@@ -1,5 +1,9 @@
 package uk.nhs.prm.deductions.pdsadaptor.auth;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.JOSEException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,36 +12,52 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Service
 public class AuthService {
 
-
     private final SignedJWTGenerator signedJWTGenerator;
-    private RestTemplate restTemplate;
-    private String accessTokenEndpoint;
+    private final RestTemplate restTemplate;
+    private final String accessTokenEndpoint;
 
-    public AuthService (SignedJWTGenerator signedJWTGenerator,RestTemplate restTemplate, @Value("${accessTokenEndpoint}") String accessTokenEndpoint){
+    public AuthService(SignedJWTGenerator signedJWTGenerator, RestTemplate restTemplate, @Value("${accessTokenEndpoint}") String accessTokenEndpoint) {
         this.signedJWTGenerator = signedJWTGenerator;
         this.restTemplate = restTemplate;
         this.accessTokenEndpoint = accessTokenEndpoint;
     }
 
-    public ResponseEntity getAccessToken() throws Exception {
+    public String getAccessToken() throws Exception {
+        try {
+            HttpEntity<MultiValueMap<String, String>> request = createRequestEntity();
+            ResponseEntity<String> accessTokenResponse = restTemplate.postForEntity(accessTokenEndpoint, request, String.class);
+
+            return getAccessToken(accessTokenResponse);
+
+        } catch (HttpClientErrorException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> createRequestEntity() throws IOException, JOSEException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        String token = signedJWTGenerator.createSignedJWT();
-        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("grant_type", "client_credentials");
         map.add("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
-        map.add("client_assertion",  token);
+        map.add("client_assertion", signedJWTGenerator.createSignedJWT());
 
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        return new HttpEntity<>(map, headers);
+    }
 
-        ResponseEntity<String> accessToken = restTemplate.postForEntity( accessTokenEndpoint, request , String.class );
+    private String getAccessToken(ResponseEntity<String> accessTokenResponse) throws JsonProcessingException {
 
-        return accessToken;
+        JsonNode parent = new ObjectMapper().readTree(accessTokenResponse.getBody());
+        return parent.get("access_token").asText();
     }
 
 }
--- src/test/java/uk/nhs/prm/deductions/pdsadaptor/auth/AuthServiceTest.java
+++ src/test/java/uk/nhs/prm/deductions/pdsadaptor/auth/AuthServiceTest.java
@@ -5,41 +5,77 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import uk.nhs.prm.deductions.pdsadaptor.auth.AuthService;
-import uk.nhs.prm.deductions.pdsadaptor.auth.SignedJWTGenerator;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
 
     @Mock
-    SignedJWTGenerator signedJWTGenerator ;
+    SignedJWTGenerator signedJWTGenerator;
 
     @Mock
     RestTemplate restTemplate;
 
     @Test
-    public void shouldReturnAccessTokenWithSignedJWT() throws Exception {
+    public void shouldRequestAccessTokenWithSignedJWT() throws Exception {
+
+        String tokenResponse = "{\"access_token\": \"Sr5PGv19wTEHJdDr2wx2f7IGd0cw\",\n" +
+                " \"expires_in\": \"599\",\n" +
+                " \"token_type\": \"Bearer\"}";
+
         Mockito.when(signedJWTGenerator.createSignedJWT()).thenReturn("Test");
-        AuthService authService = new AuthService(signedJWTGenerator,restTemplate,"https://token-endpoint");
+        AuthService authService = new AuthService(signedJWTGenerator, restTemplate, "https://token-endpoint");
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("grant_type", "client_credentials");
         map.add("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
-        map.add("client_assertion",  "Test");
+        map.add("client_assertion", "Test");
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-        ResponseEntity<String> accessToken = authService.getAccessToken();
-        Mockito.verify(restTemplate).postForEntity("https://token-endpoint",request,String.class);
+
+        Mockito.when((restTemplate).postForEntity("https://token-endpoint", request, String.class)).thenReturn(new ResponseEntity<String>(tokenResponse, HttpStatus.OK));
+
+        String accessToken = authService.getAccessToken();
+
+        Mockito.verify(restTemplate).postForEntity("https://token-endpoint", request, String.class);
+        assertThat(accessToken).isEqualTo("Sr5PGv19wTEHJdDr2wx2f7IGd0cw");
+
+    }
+
+    @Test
+    public void shouldHandleFailureFromTokenAccessRequestEndPoint() throws Exception {
+
+        String tokenResponse = "{\"error\": \"invalid_request\",\n" +
+                " \"error_description\": \"Error response\",\n" +
+                " \"message_id\": \"error\"}";
+
+        Mockito.when(signedJWTGenerator.createSignedJWT()).thenReturn("Test");
+        AuthService authService = new AuthService(signedJWTGenerator, restTemplate, "https://token-endpoint");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "client_credentials");
+        map.add("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
+        map.add("client_assertion", "Test");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        Mockito.when((restTemplate).postForEntity("https://token-endpoint", request, String.class)).thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
+
+        String accessTokenResponse = authService.getAccessToken();
+
+        Mockito.verify(restTemplate).postForEntity("https://token-endpoint", request, String.class);
+//        assertThat(accessTokenResponse).isEqualTo(null);
 
     }
 
