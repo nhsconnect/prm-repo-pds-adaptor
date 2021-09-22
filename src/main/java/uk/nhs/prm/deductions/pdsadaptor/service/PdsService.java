5ed935737c523c6c6129910af6f348da8c8f9c15
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-09-22 13:52:57 +0100

    [PRMT-2101] Refactored PDS Service to account for no GP Practitioner for suspended patients

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/service/PdsService.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/service/PdsService.java
@@ -19,24 +19,26 @@ public class PdsService {
     public SuspendedPatientStatus getPatientGpStatus(String nhsNumber) {
         SuspendedPatientStatus suspendedPatientStatus = new SuspendedPatientStatus();
         PdsResponse pdsResponse = pdsFhirClient.requestPdsRecordByNhsNumber(nhsNumber);
-        GeneralPractitioner generalPractitioner = getGeneralPractitioner(pdsResponse);
-        String odsCode = generalPractitioner.getIdentifier().getValue();
-        LocalDate gpEndDate = generalPractitioner.getIdentifier().getPeriod().getEnd();
-        if (gpEndDate != null && gpEndDate.isBefore(LocalDate.now())) {
-            suspendedPatientStatus.setPreviousOdsCode(odsCode);
-            suspendedPatientStatus.setIsSuspended(true);
-        } else {
+
+        boolean hasGeneralPractitioner = hasGeneralPractitioner(pdsResponse);
+
+        if (hasGeneralPractitioner) {
+            String odsCode = getOdsCode(pdsResponse);
             suspendedPatientStatus.setCurrentOdsCode(odsCode);
             suspendedPatientStatus.setIsSuspended(false);
+        } else {
+            suspendedPatientStatus.setIsSuspended(true);
         }
 
         return suspendedPatientStatus;
     }
 
-    private GeneralPractitioner getGeneralPractitioner(PdsResponse pdsResponse) {
-        if (pdsResponse.getGeneralPractitioner() == null || pdsResponse.getGeneralPractitioner().size() == 0) {
-            throw new NotFoundException("Unable to process request - GP Practice missing from response");
-        }
-        return pdsResponse.getGeneralPractitioner().get(0);
+    private String getOdsCode(PdsResponse pdsResponse) {
+        GeneralPractitioner generalPractitioner = pdsResponse.getGeneralPractitioner().get(0);
+        return generalPractitioner.getIdentifier().getValue();
+    }
+
+    private boolean hasGeneralPractitioner(PdsResponse pdsResponse) {
+        return pdsResponse.getGeneralPractitioner() != null && pdsResponse.getGeneralPractitioner().size() != 0;
     }
 }
--- src/test/java/uk/nhs/prm/deductions/pdsadaptor/service/PdsServiceTest.java
+++ src/test/java/uk/nhs/prm/deductions/pdsadaptor/service/PdsServiceTest.java
@@ -1,22 +1,18 @@
 package uk.nhs.prm.deductions.pdsadaptor.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.nhs.prm.deductions.pdsadaptor.client.PdsFhirClient;
-import uk.nhs.prm.deductions.pdsadaptor.model.Exceptions.NotFoundException;
 import uk.nhs.prm.deductions.pdsadaptor.model.pdsresponse.PdsResponse;
 import uk.nhs.prm.deductions.pdsadaptor.model.SuspendedPatientStatus;
 import uk.nhs.prm.deductions.pdsadaptor.testhelpers.TestData;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,13 +23,13 @@ class PdsServiceTest {
 
     @InjectMocks
     private PdsService pdsService;
+    public static final String NHS_NUMBER = "1234567890";
 
     @Test
     void shouldCallPdsFhirClientAndReturnResponseForNonSuspendedPatient() {
-        String nhsNumber = "1234";
-        PdsResponse pdsResponse = TestData.buildPdsResponse(nhsNumber, "B1234", LocalDate.now().minusYears(1), null);
-        when(pdsFhirClient.requestPdsRecordByNhsNumber(nhsNumber)).thenReturn(pdsResponse);
-        SuspendedPatientStatus expected = pdsService.getPatientGpStatus(nhsNumber);
+        PdsResponse pdsResponse = TestData.buildPdsResponse(NHS_NUMBER, "B1234", LocalDate.now().minusYears(1), null);
+        when(pdsFhirClient.requestPdsRecordByNhsNumber(NHS_NUMBER)).thenReturn(pdsResponse);
+        SuspendedPatientStatus expected = pdsService.getPatientGpStatus(NHS_NUMBER);
 
         assertThat(expected.getIsSuspended()).isEqualTo(false);
         assertThat(expected.getCurrentOdsCode()).isEqualTo("B1234");
@@ -41,48 +37,12 @@ class PdsServiceTest {
     }
 
     @Test
-    void shouldCallPdsFhirClientAndReturnResponseForSuspendedPatients() {
-        String nhsNumber = "1234";
-        PdsResponse pdsResponse = TestData.buildPdsResponse(nhsNumber, "B1234", LocalDate.now().minusYears(1), LocalDate.now().minusMonths(1));
-        when(pdsFhirClient.requestPdsRecordByNhsNumber(nhsNumber)).thenReturn(pdsResponse);
-        SuspendedPatientStatus expected = pdsService.getPatientGpStatus(nhsNumber);
+    void shouldReturnSuspendedPatientResponseWhenNoGpPractitionerField() {
+        PdsResponse pdsResponse = new PdsResponse(NHS_NUMBER, null);
+        when(pdsFhirClient.requestPdsRecordByNhsNumber(NHS_NUMBER)).thenReturn(pdsResponse);
+
+        SuspendedPatientStatus expected = pdsService.getPatientGpStatus(NHS_NUMBER);
 
         assertThat(expected.getIsSuspended()).isEqualTo(true);
-        assertThat(expected.getCurrentOdsCode()).isNullOrEmpty();
-        assertThat(expected.getPreviousOdsCode()).isEqualTo("B1234");
-    }
-
-    @Test
-    void shouldCallPdsFhirClientAndReturnResponseForNonSuspendedPatientWithEndDate() {
-        String nhsNumber = "1234";
-        PdsResponse pdsResponse = TestData.buildPdsResponse(nhsNumber, "B1234", LocalDate.now().minusYears(1), LocalDate.now().plusMonths(1));
-        when(pdsFhirClient.requestPdsRecordByNhsNumber(nhsNumber)).thenReturn(pdsResponse);
-        SuspendedPatientStatus expected = pdsService.getPatientGpStatus(nhsNumber);
-
-        assertThat(expected.getIsSuspended()).isEqualTo(false);
-        assertThat(expected.getCurrentOdsCode()).isEqualTo("B1234");
-        assertThat(expected.getPreviousOdsCode()).isNullOrEmpty();
-    }
-
-    @Test
-    void shouldThrowAnErrorWhenNoGpPractitionerField() {
-        String nhsNumber = "1234";
-        PdsResponse pdsResponse = new PdsResponse(nhsNumber, null);
-        when(pdsFhirClient.requestPdsRecordByNhsNumber(nhsNumber)).thenReturn(pdsResponse);
-
-        Exception exception = assertThrows(NotFoundException.class, () -> pdsService.getPatientGpStatus(nhsNumber));
-
-        Assertions.assertThat(exception.getMessage()).isEqualTo("Unable to process request - GP Practice missing from response");
-    }
-
-    @Test
-    void shouldThrowAnErrorWhenEmptyGpPractitionerField() {
-        String nhsNumber = "1234";
-        PdsResponse pdsResponse = new PdsResponse(nhsNumber, List.of());
-        when(pdsFhirClient.requestPdsRecordByNhsNumber(nhsNumber)).thenReturn(pdsResponse);
-
-        Exception exception = assertThrows(NotFoundException.class, () -> pdsService.getPatientGpStatus(nhsNumber));
-
-        Assertions.assertThat(exception.getMessage()).isEqualTo("Unable to process request - GP Practice missing from response");
     }
 }
