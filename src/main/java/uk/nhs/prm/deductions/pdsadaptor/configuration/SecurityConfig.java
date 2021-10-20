10fd1439f8a55fd1ccc1374ae1160aac1974ea69
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-10-20 14:19:12 +0100

    [PRMT-2165] Enabled statelessness for authentication so no cookies

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/SecurityConfig.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import software.amazon.awssdk.regions.Region;
@@ -90,7 +91,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().
+        http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
                 httpBasic().and().
                 authorizeRequests().
                 antMatchers(AUTH_WHITELIST).permitAll().
