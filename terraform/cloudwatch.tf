698bff72b1926d2e6acba1cd9990ed4ca1855e45
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-11-24 12:12:37 +0000

    [PRMT-2313] Created log metric filter and alarm for errors

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -6,3 +6,30 @@ resource "aws_cloudwatch_log_group" "log_group" {
     CreatedBy= var.repo_name
   }
 }
+
+resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
+  name           = "${var.component_name}-error-log-metric-filter"
+  pattern        = "{ $.level = \"ERROR\" }"
+  log_group_name = aws_cloudwatch_log_group.log_group.name
+
+  metric_transformation {
+    name          = "ErrorCountInLogs"
+    namespace     = "PdsAdaptor"
+    value         = 1
+    default_value = 0
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "error_log_alarm" {
+  alarm_name                = "pds-adaptor-error-log-alarm"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "1"
+  period                    = "60"
+  metric_name               = aws_cloudwatch_log_metric_filter.log_metric_filter.metric_transformation[0].name
+  namespace                 = aws_cloudwatch_log_metric_filter.log_metric_filter.metric_transformation[0].namespace
+  statistic                 = "Sum"
+  threshold                 = "0"
+  unit                      = "Count"
+  alarm_description         = "This alarm monitors errors logs in PDS Adaptor"
+  treat_missing_data        = "missing"
+}
