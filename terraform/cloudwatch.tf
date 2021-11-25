942b609388db4c0e0f979f3e073eb64bfed8dfe4
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-11-25 16:17:18 +0000

    [PRMT-2313] Updated error log alarm to treat missing data as not breaching

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,3 +1,8 @@
+locals {
+  error_logs_metric_name       = "ErrorCountInLogs"
+  pds_adaptor_metric_namespace = "PdsAdaptor"
+}
+
 resource "aws_cloudwatch_log_group" "log_group" {
   name = "/nhs/deductions/${var.environment}-${data.aws_caller_identity.current.account_id}/${var.component_name}"
 
@@ -13,8 +18,8 @@ resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
   log_group_name = aws_cloudwatch_log_group.log_group.name
 
   metric_transformation {
-    name          = "ErrorCountInLogs"
-    namespace     = "PdsAdaptor"
+    name          = local.error_logs_metric_name
+    namespace     = local.pds_adaptor_metric_namespace
     value         = 1
     default_value = 0
   }
@@ -26,11 +31,11 @@ resource "aws_cloudwatch_metric_alarm" "error_log_alarm" {
   threshold                 = "0"
   evaluation_periods        = "1"
   period                    = "60"
-  metric_name               = aws_cloudwatch_log_metric_filter.log_metric_filter.metric_transformation[0].name
-  namespace                 = aws_cloudwatch_log_metric_filter.log_metric_filter.metric_transformation[0].namespace
+  metric_name               = local.error_logs_metric_name
+  namespace                 = local.pds_adaptor_metric_namespace
   statistic                 = "Sum"
   alarm_description         = "This alarm monitors errors logs in ${var.component_name}"
-  treat_missing_data        = "missing"
+  treat_missing_data        = "notBreaching"
   actions_enabled           = "true"
   alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
 }
