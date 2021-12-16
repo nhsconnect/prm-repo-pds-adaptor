61f0e3b03cc8809529a7e1570cf4c9429b513fc6
Dan Moore <dan.moore1@nhs.net>
2021-12-16 17:17:11 +0000

    Fail fast if PDS_API_KEY not set

--- pds-app-auth-spike/config.py
+++ pds-app-auth-spike/config.py
@@ -6,10 +6,10 @@ from typing import Optional
 
 @dataclass
 class Config:
-    api_key: Optional[str] = os.getenv('PDS_API_KEY')
+    api_key: Optional[str] = os.environ['PDS_API_KEY']
     pds_fhir_url: Optional[str] = 'https://int.api.service.nhs.uk/personal-demographics/FHIR/R4/'
     nhs_env: Optional[str] = 'int'
-    patient_nhs_number: str = '9692295966'
+    patient_nhs_number: str = '9693797515'
 
 
 def read_config_from_env():
