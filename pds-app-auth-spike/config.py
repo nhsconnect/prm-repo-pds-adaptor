971a07c8907defb5d7332b1abb78f5bd9f712ff6
Dan Moore <dan.moore1@nhs.net>
2021-12-20 13:04:27 +0000

    Pass in NHS_NUMBER for patient

--- pds-app-auth-spike/config.py
+++ pds-app-auth-spike/config.py
@@ -9,7 +9,7 @@ class Config:
     api_key: Optional[str] = os.environ['PDS_API_KEY']
     pds_fhir_url: Optional[str] = 'https://int.api.service.nhs.uk/personal-demographics/FHIR/R4/'
     nhs_env: Optional[str] = 'int'
-    patient_nhs_number: str = '9693797515'
+    patient_nhs_number: str = os.environ['NHS_NUMBER'] 
 
 
 def read_config_from_env():
