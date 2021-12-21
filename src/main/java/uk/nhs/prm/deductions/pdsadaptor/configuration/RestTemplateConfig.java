d6759b2bd82a3177a30fde0de4af38c05cca6d34
Jack Slater <jack.slater1@nhs.net>
2021-12-21 16:23:23 +0000

    [PRMT-2102] enable patch requests on the rest template

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package uk.nhs.prm.deductions.pdsadaptor.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import uk.nhs.prm.deductions.pdsadaptor.client.auth.AuthService;
 import uk.nhs.prm.deductions.pdsadaptor.client.auth.OAuthRequestInterceptor;
@@ -20,7 +21,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate pdsFhirRestTemplate(AuthService authService) {
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
         restTemplate.setInterceptors(List.of(new OAuthRequestInterceptor(authService)));
         return restTemplate;
     }
