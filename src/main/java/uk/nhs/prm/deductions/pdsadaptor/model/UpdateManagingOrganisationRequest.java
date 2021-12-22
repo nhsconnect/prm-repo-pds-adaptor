d43028535b7f0902c36a650de6f02926ae238662
Jack Slater <jack.slater1@nhs.net>
2021-12-22 11:44:42 +0000

    [PRMT-2102] add required validation on body of update request

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/model/UpdateManagingOrganisationRequest.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/model/UpdateManagingOrganisationRequest.java
@@ -1,15 +1,26 @@
 package uk.nhs.prm.deductions.pdsadaptor.model;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateManagingOrganisationRequest {
+    @NotNull
+    @NotBlank
+    @Parameter(required = true)
     private String previousGp;
+
+    @NotNull
+    @NotBlank
+    @Parameter(required = true)
     private String recordETag;
 }
