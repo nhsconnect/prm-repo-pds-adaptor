73905655d22c8686aa6d2e9af1355d75c8789df9
Rosie Amphlett <rosie.amphlett1@nhs.net>
2022-01-14 16:57:45 +0000

    [PRMT-1842] Added 'Basic' to auth key for performance testing

--- src/performance/test_performance.py
+++ src/performance/test_performance.py
@@ -8,7 +8,7 @@ class PDSAdaptorPerformanceTest(HttpUser):
     @task
     def suspended_patient_status(self):
         patient_id = "9693797523"
-        self.client.headers = {"Authorization": self.generate_api_key()}
+        self.client.headers = { "Authorization" : self.generate_api_key() }
         response = self.client.get(f"/suspended-patient-status/{patient_id}")
         # TODO: use values for organisation_field_update call?
         # print(response.json())
@@ -16,16 +16,20 @@ class PDSAdaptorPerformanceTest(HttpUser):
     @task
     def organisation_field_update(self):
         patient_id = "9693797523"
-        previous_gp = "Old GP"
+        previous_gp = "OldGP"
         record_e_tag = "An ETag"
-        self.client.headers = {"Authorization": self.generate_api_key()}
+        self.client.headers = { "Authorization" : self.generate_api_key() }
         data = { "previousGp": previous_gp, "recordETag": record_e_tag }
-        self.client.put(url=f"/suspended-patient-status/{patient_id}", data=data)
+        self.client.put(url=f"/suspended-patient-status/{patient_id}", json=data)
 
     def generate_api_key(self):
+        api_key_ssm_parameter = self.get_api_key_from_ssm()
+        username = "performance-test"
+        password = api_key_ssm_parameter["Parameter"]["Value"]
+        encoded_key = b64encode(f"{username}:{password}".encode("utf8")).decode("ascii")
+        return f"Basic {encoded_key}"
+
+    def get_api_key_from_ssm(self):
         env = os.environ["NHS_ENVIRONMENT"]
         ssm_parameter_name = f"/repo/{env}/user-input/api-keys/pds-adaptor/performance-test"
-        boto_response = boto3.client('ssm').get_parameter(Name=ssm_parameter_name, WithDecryption=True)
-        username = "performance-test"
-        password = boto_response["Parameter"]["Value"]
-        return b64encode(f"{username}:{password}".encode("utf8")).decode("ascii")
+        return boto3.client('ssm').get_parameter(Name=ssm_parameter_name, WithDecryption=True)
