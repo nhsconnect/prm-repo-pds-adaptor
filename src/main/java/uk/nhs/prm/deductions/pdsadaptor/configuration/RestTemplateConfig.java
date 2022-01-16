8391add029dae46a75ae820c580b4e3dd85ef3f6
Jack Slater <jack.slater1@nhs.net>
2022-01-16 20:12:18 +0000

    connection-factory fix

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
@@ -1,9 +1,15 @@
 package uk.nhs.prm.deductions.pdsadaptor.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import uk.nhs.prm.deductions.pdsadaptor.client.auth.AuthService;
 import uk.nhs.prm.deductions.pdsadaptor.client.auth.OAuthRequestInterceptor;
@@ -20,12 +26,21 @@ public class RestTemplateConfig {
     }
 
     @Bean
-    public RestTemplate pdsFhirRestTemplate(AuthService authService) {
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(30000);
-        requestFactory.setReadTimeout(30000);
+    public RestTemplate pdsFhirRestTemplate(ClientHttpRequestFactory requestFactory, AuthService authService) {
         RestTemplate restTemplate = new RestTemplate(requestFactory);
         restTemplate.setInterceptors(List.of(new OAuthRequestInterceptor(authService)));
         return restTemplate;
+    }
+
+    @Bean
+    public ClientHttpRequestFactory createRequestFactory() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(50);
+
+        RequestConfig config = RequestConfig.custom().setConnectTimeout(100000).build();
+        CloseableHttpClient httpClient = HttpClientBuilder.create().setConnectionManager(connectionManager)
+            .setDefaultRequestConfig(config).build();
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 }
