c3da9b77bfa5d4ff26e5e8277a62ae1e345a360e
Dan Moore <dan.moore1@nhs.net>
2022-01-17 17:03:48 +0000

    [PRMT-1842] Inline GET for last ETag into PUT perf task
    Will change counts against GET call but much simpler
    w.r.t state and ordering of tasks

--- src/performance/test_performance.py
+++ src/performance/test_performance.py
@@ -2,6 +2,11 @@ from base64 import b64encode
 from locust import task, between, FastHttpUser
 import boto3, os
 
+RECORD_ETAG_NAME='recordETag'
+
+def extract_etag(response_data):
+    return response_data[RECORD_ETAG_NAME]
+
 class PdsAdaptorUser(FastHttpUser):
     wait_time = between(0.5, 1)
     connection_timeout = 10
@@ -10,19 +15,34 @@ class PdsAdaptorUser(FastHttpUser):
     @task
     def suspended_patient_status(self):
         patient_id = "9693797523"
-        headers = { "Authorization" : self.generate_api_key() }
-        response = self.client.get(f"/suspended-patient-status/{patient_id}", headers=headers)
-        # TODO: use values for organisation_field_update call?
-        # print(response.json())
+
+        status_data = self._get_suspended_patient_status(patient_id)
+        print('status_data', status_data)
 
     @task
     def organisation_field_update(self):
         patient_id = "9693797523"
+
+        status_data = self._get_suspended_patient_status(patient_id)
+        last_etag = extract_etag(status_data)
+
         previous_gp = "OldGP"
-        record_e_tag = "An ETag"
         headers = { "Authorization" : self.generate_api_key() }
-        data = { "previousGp": previous_gp, "recordETag": record_e_tag }
+        data = { 
+            "previousGp": previous_gp, 
+            RECORD_ETAG_NAME: last_etag 
+        }
         self.client.put(f"/suspended-patient-status/{patient_id}", json=data, headers=headers)
+
+
+    def _get_suspended_patient_status(self, patient_id):
+        headers = { "Authorization" : self.generate_api_key() }
+        response = self.client.get(f"/suspended-patient-status/{patient_id}", headers=headers)
+
+        response_data = response.json()
+        print('response_data', response_data)
+
+        return response_data
 
     def generate_api_key(self):
         api_key_ssm_parameter = self.get_api_key_from_ssm()
