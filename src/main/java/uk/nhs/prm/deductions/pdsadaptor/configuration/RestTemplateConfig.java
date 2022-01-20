43e05066aff54e6ac4191f2dd93f90f23e3c31fc
Dan Moore <dan.moore1@nhs.net>
2022-01-20 18:08:35 +0000

    [PRMT-2511] Remove now unused auth interceptor

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import uk.nhs.prm.deductions.pdsadaptor.client.auth.AuthService;
-import uk.nhs.prm.deductions.pdsadaptor.client.auth.OAuthRequestInterceptor;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
