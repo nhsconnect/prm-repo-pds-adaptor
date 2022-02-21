f0a4416d69e9880d857030f6c56c1d4e636bc0bb
Dan Moore <dan.moore1@nhs.net>
2022-02-21 16:38:35 +0000

    Revert "[PRMT-2471] Set http connection pool size 50 to have concurrent connections to fhir api."
    This reverts commit be7238f904a0e86f9aca547059d72bfb91097941.

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/configuration/RestTemplateConfig.java
@@ -1,7 +1,6 @@
 package uk.nhs.prm.deductions.pdsadaptor.configuration;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -20,7 +19,6 @@ public class RestTemplateConfig {
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
         requestFactory.setConnectTimeout(30000);
         requestFactory.setReadTimeout(30000);
-        requestFactory.setHttpClient(HttpClients.custom().setMaxConnTotal(50).setMaxConnPerRoute(50).build());
         return requestFactory;
     }
 }
