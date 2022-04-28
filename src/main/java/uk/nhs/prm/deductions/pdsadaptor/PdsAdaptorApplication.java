7c216e0f67c7ec5a9623c310c5784711ba5adb45
Dan Moore <dan.moore1@nhs.net>
2022-04-28 09:34:21 +0100

    [PRMT-2632] Remove structured log spike

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/PdsAdaptorApplication.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/PdsAdaptorApplication.java
@@ -1,37 +1,12 @@
 package uk.nhs.prm.deductions.pdsadaptor;
 
-import lombok.extern.slf4j.Slf4j;
-import net.logstash.logback.marker.Markers;
-import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import java.util.HashMap;
-
-import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @SpringBootApplication
-@Slf4j
 public class PdsAdaptorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PdsAdaptorApplication.class, args);
     }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void afterStartup() {
-        var map = new HashMap<String, String>();
-        var superMap = new HashMap<String, Object>();
-        map.put("what", "spring");
-        map.put("evaluation", "great");
-        superMap.put("nested", map);
-
-        var superJson = new JSONObject(superMap).toString();
-
-        log.info(Markers.appendRaw("detail", superJson), "log spike: detail from json string");
-        log.info(Markers.append("detail", superMap), "log spike: detail from map of info");
-    }
-
 }
