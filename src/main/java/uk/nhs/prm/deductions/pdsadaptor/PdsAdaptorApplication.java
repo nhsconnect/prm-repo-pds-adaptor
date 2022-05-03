aacad97a2b35afb7bb7064182f00895eaee5a649
Dan Moore <dan.moore1@nhs.net>
2022-05-03 18:01:43 +0100

    Revert "[PRMT-2632] Attempted JSON log fix spike"
    This reverts commit c723f30cc8a6c7192c22ba5d1850a7ac3256d22f.

--- src/main/java/uk/nhs/prm/deductions/pdsadaptor/PdsAdaptorApplication.java
+++ src/main/java/uk/nhs/prm/deductions/pdsadaptor/PdsAdaptorApplication.java
@@ -1,7 +1,5 @@
 package uk.nhs.prm.deductions.pdsadaptor;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import net.logstash.logback.marker.Markers;
 import org.json.JSONObject;
@@ -34,25 +32,12 @@ public class PdsAdaptorApplication {
 
         log.info(Markers.appendRaw("detail", superJson), "log spike: detail from json string");
 
-        var jsonWithBreaks = "{\"a\":\"aa\"," +
+        var superJsonWithBreaks = "{\"a\":\"aa\"," +
                 "\n\"b\":{" +
                     "\n\"c\":\"cc\"}" +
                 "\n}";
 
-        log.info(Markers.appendRaw("detail", jsonWithBreaks), "log spike: detail from json with line breaks");
-        log.info(Markers.append("detail", parseJson(jsonWithBreaks)), "log spike: re-parsed to remove line breaks");
-        log.info(Markers.append("detail", parseJson(superJson)), "log spike: re-parsed original superJson");
-    }
-
-    private HashMap<String, Object> parseJson(String json) {
-        try {
-            return new ObjectMapper().readValue(json, HashMap.class);
-        }
-        catch (JsonProcessingException e) {
-            return new HashMap<>() {{
-                put("json_parse_failure", e.getMessage());
-            }};
-        }
+        log.info(Markers.appendRaw("detail", superJsonWithBreaks), "log spike: detail from json with line breaks");
     }
 
 }
