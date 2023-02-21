8d7ccb88a74059c51c689005e5b62163e7e44ca0
Dan Moore <dan.moore1@nhs.net>
2023-02-21 14:21:49 +0000

    [PRMT-3168] Update terraform to allow used of moved on cert

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  profile   = "default"
   region    = var.region
 }
 
 terraform {
+  required_version = ">= 1.3.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.44.0"
+      version = "4.48.0"
     }
   }
 }
